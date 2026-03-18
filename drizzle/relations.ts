d52d4c721843120623193b10ba5fbff02acfd619
FriNite <dcchie217@gmail.com>
2026-03-18 19:53:47 +0900

    repair

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,14 +1,14 @@
 import { relations } from "drizzle-orm/relations";
 
-import { stationRecords,stations } from "./schema";
+import { stationRecords, stations } from "../lib/schema";
 
-export const stationRecordsRelations = relations(stationRecords, ({one}) => ({
+export const stationRecordsRelations = relations(stationRecords, ({ one }) => ({
 	station: one(stations, {
-		fields: [stationRecords.stationId],
-		references: [stations.stationId]
+		fields: [stationRecords.station_id],
+		references: [stations.station_id]
 	}),
 }));
 
-export const stationsRelations = relations(stations, ({many}) => ({
+export const stationsRelations = relations(stations, ({ many }) => ({
 	stationRecords: many(stationRecords),
 }));
